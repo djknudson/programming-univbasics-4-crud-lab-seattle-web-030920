264c9aa45897f78665d05bdd891314d22d76067f
David Knudson <djknudson@icloud.com>
2020-02-26 09:13:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  empty_array = Array.new
+  return empty_array
 end
 
 def create_an_array
-  
+  array = ["apple", "banana", "cherry", "dragon fruit"]
+  return array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  return array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(elememnt)
+  return array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
+  return array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
+  return array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.delete_at(index_number)
+  return array
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array[array.length() - 1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
+  return array
 end
