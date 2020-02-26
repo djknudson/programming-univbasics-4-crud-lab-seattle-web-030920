d0548384c9fdddbee57e43d434fd93ac63b97277
David Knudson <djknudson@icloud.com>
2020-02-26 09:20:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,22 +15,18 @@ end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  # return array
 end
 
 def remove_element_from_end_of_array(array)
   array.pop()
-  # return array
 end
 
 def remove_element_from_start_of_array(array)
   array.shift()
-  # return array
 end
 
 def retrieve_element_from_index(array, index_number)
   array.delete_at(index_number)
-  # return array
 end
 
 def retrieve_first_element_from_array(array)
@@ -43,5 +39,4 @@ end
 
 def update_element_from_index(array, index_number, element)
   array[index_number] = element
-  # return array
 end
